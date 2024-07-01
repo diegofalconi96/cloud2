b46bdf731f35f02e9a0259c9bcf6bd92d2a7f14d
DESKTOP-51GC2CI\Danny <danny>
2024-07-01 11:31:04 -0500

    Clound v2

--- simba_danny/CLOUD2/index.js
+++ simba_danny/CLOUD2/index.js
@@ -6,7 +6,7 @@ const routes=require('./routes')//llamada a routes
 const app=express();//creamos nueva instancia 
 const port=3000;//puerto de salida
 
-app.use(express.json());//configura tipo json
+app.use(express.json());//  configura tipo json
 
 app.use('/api',routes);//configura la url base y rutas
 
