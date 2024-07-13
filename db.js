befe0b573911ddd9b3136386096c4c5d6388dbdc
RomelAnte <rsag20092002@gmail.com>
2024-07-12 20:43:42 -0500

    nuevos cambios

--- db.js
+++
@@ -1,8 +0,0 @@
-const Knex = require('knex') 
-const {Model} = require('objection')
-const knexConfig = require('./knexfile')
-
-const knex = Knex(knexConfig.development);
-Model.knex(knex);
-
-module.exports = knex;
